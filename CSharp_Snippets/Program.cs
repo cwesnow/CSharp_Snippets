dd371b84391a987fa597a5a316b381766b1fc12e
cwesnow2000 <cwesnow2000@yahoo.com>
2016-10-10 15:26:38 -0500

    First Commit

---
+++ CSharp_Snippets/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CSharp_Snippets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "C# Snippets";
+            Console.WriteLine("\nWelcome to C#\n");
+        }
+    }
+}
